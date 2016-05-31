bb87ed30c237f3f8260547b55c8fd48f7fad601a
Magnus Bergman <karlmagnusbergman@gmail.com>
2016-05-31 20:22:17 +0200

    :rotating_light: Add more tests for actions.

--- client/scripts/actions/__tests__/actions.test.js
+++ client/scripts/actions/__tests__/actions.test.js
@@ -70,68 +70,68 @@ test.serial('Action:loginUser dispatches LOGIN_REQUEST and LOGIN_SUCCESS on succ
     })
 })
 
-// test.serial('Action:getUser  dispatches GET_USER_REQUEST and GET_USER_SUCCESS on success', t => {
-//   t.plan(1)
-//
-//   const successResponse = {
-//     token: 'xxxxxxxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx',
-//     user: {
-//       id: 1,
-//       username: 'john',
-//       firstName: 'John',
-//       lastName: 'Doe'
-//     }
-//   }
-//
-//   const successResponseObj = new window.Response(JSON.stringify(successResponse), {
-//     status: 200,
-//     headers: { 'Content-type': 'application/json' }
-//   })
-//
-//   window.fetch.returns(Promise.resolve(successResponseObj))
-//
-//   const expectedActions = [
-//     { type: actions.GET_USER_REQUEST },
-//     {
-//       type: actions.GET_USER_SUCCESS,
-//       payload: successResponse
-//     }
-//   ]
-//
-//   const token = 'xxxxxxxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx'
-//
-//   return store
-//     .dispatch(actions.getUser(token))
-//     .then(() => {
-//       t.deepEqual(store.getActions(), expectedActions)
-//     })
-// })
-//
-// test.serial('Action:logoutUser dispatches LOGOUT_REQUEST and LOGOUT_SUCCESS on success', t => {
-//   t.plan(1)
-//
-//   const successResponse = {}
-//
-//   const successResponseObj = new window.Response(JSON.stringify(successResponse), {
-//     status: 204,
-//     headers: { 'Content-type': 'application/json' }
-//   })
-//
-//   window.fetch.returns(Promise.resolve(successResponseObj))
-//
-//   const expectedActions = [
-//     { type: actions.LOGOUT_REQUEST },
-//     {
-//       type: actions.LOGOUT_SUCCESS,
-//       payload: successResponse
-//     }
-//   ]
-//
-//   const token = 'xxxxxxxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx'
-//
-//   return store
-//     .dispatch(actions.logoutUser(token))
-//     .then(() => {
-//       t.deepEqual(store.getActions(), expectedActions)
-//     })
-// })
+test.serial('Action:getUser dispatches GET_USER_REQUEST and GET_USER_SUCCESS on success', t => {
+  t.plan(1)
+
+  const successResponse = {
+    token: 'xxxxxxxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx',
+    user: {
+      id: 1,
+      username: 'john',
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+  }
+
+  const successResponseObj = new window.Response(JSON.stringify(successResponse), {
+    status: 200,
+    headers: { 'Content-type': 'application/json' }
+  })
+
+  window.fetch.returns(Promise.resolve(successResponseObj))
+
+  const expectedActions = [
+    { type: actions.GET_USER_REQUEST },
+    {
+      type: actions.GET_USER_SUCCESS,
+      payload: successResponse
+    }
+  ]
+
+  const token = 'xxxxxxxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx'
+
+  return store
+    .dispatch(actions.getUser(token))
+    .then(() => {
+      t.deepEqual(store.getActions(), expectedActions)
+    })
+})
+
+test.serial('Action:logoutUser dispatches LOGOUT_REQUEST and LOGOUT_SUCCESS on success', t => {
+  t.plan(1)
+
+  const successResponse = {}
+
+  const successResponseObj = new window.Response(JSON.stringify(successResponse), {
+    status: 204,
+    headers: { 'Content-type': 'application/json' }
+  })
+
+  window.fetch.returns(Promise.resolve(successResponseObj))
+
+  const expectedActions = [
+    { type: actions.LOGOUT_REQUEST },
+    {
+      type: actions.LOGOUT_SUCCESS,
+      payload: successResponse
+    }
+  ]
+
+  const token = 'xxxxxxxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx'
+
+  return store
+    .dispatch(actions.logoutUser(token))
+    .then(() => {
+      t.deepEqual(store.getActions(), expectedActions)
+    })
+})
